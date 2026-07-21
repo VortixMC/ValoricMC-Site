a4032536c0ded82d2cbe46d9fee7bcadddba8fb2
Sebminecrafter <116523841+Sebminecrafter@users.noreply.github.com>
2026-07-21 12:30:03 -0400

    Update placeholder-parser.js

--- assets/js/placeholder-parser.js
+++ assets/js/placeholder-parser.js
@@ -9,7 +9,7 @@ Placeholders:
 Data is fetched from the mcsrvstat.us public API
 */
 
-const MC_SERVER = "play.vortixmc.xyz:25569";
+const MC_SERVER = "play.vortixmc.xyz";
 const CACHE_TTL_MS = 30_000;
 const PLACEHOLDER_RE = /\{\{(\w+)\}\}/g;
 
